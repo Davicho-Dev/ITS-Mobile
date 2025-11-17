06bbed0c932840c23e97979a0bcaea60917b5ea4
Davicho-Dev <13292121+Davicho-Dev@users.noreply.github.com>
2025-11-17 09:14:14 -0500

    feat(error-spinner): add IcError and IcSpinner components with animations;
    Previously, there were no dedicated components for error and loading states in the UI.
    
    Now, the IcError component displays an animated error icon, while the IcSpinner component provides a rotating spinner for loading indications.
    
    These additions enhance user experience by providing visual feedback during error and loading scenarios.

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,7 @@
+export * from './Buttons/ButtonSolid/ButtonSolid';
+export * from './Form/FormField/FormField';
+export * from './Form/FormInputSearch/FormInputSearch';
+export * from './Links/LinkSolid/LinkSolid';
+export * from './Separator/Separator';
+export * from './icons/Error/Error';
+export * from './icons/Spinner/Spinner';
