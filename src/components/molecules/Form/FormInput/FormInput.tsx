5e6bd9a4ad873c6d7a538a59567493bdb55de316
Davicho-Dev <13292121+Davicho-Dev@users.noreply.github.com>
2025-11-17 09:15:15 -0500

    feat(input): create FormInput component with styles and props;
    Added a new FormInput component to handle user input with validation support.
    
    The component integrates with react-hook-form for controlled inputs, providing error handling and customizable styles.
    
    This implementation enhances form handling capabilities and improves user experience by displaying error messages and styling based on input state.

---
+++ src/components/molecules/Form/FormInput/FormInput.tsx
@@ -0,0 +1,60 @@
+import type { FC } from 'react';
+
+import type { TextInputProps } from 'react-native';
+import { TextInput } from 'react-native';
+import type { FieldValues } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+
+import { FormField } from '@atoms';
+
+import type { IFormInputProps } from './FormInput.interfaces';
+
+import { styles } from './FormInput.styles';
+
+export const FormInput = <T extends FieldValues>({
+	label,
+	hasError = false,
+	errorMessage = '',
+	placeholder = '',
+	control,
+	name,
+	rules,
+}: IFormInputProps<T>) => {
+	return (
+		<Controller
+			name={name}
+			control={control}
+			rules={rules}
+			render={({ field: { value, onChange } }) => (
+				<InputField
+					autoCapitalize='none'
+					placeholder={placeholder}
+					label={label}
+					value={value}
+					onChangeText={onChange}
+					hasError={hasError}
+					errorMessage={errorMessage}
+				/>
+			)}
+		/>
+	);
+};
+
+interface IInputFieldProps extends TextInputProps {
+	label: string;
+	hasError?: boolean;
+	errorMessage?: string;
+}
+
+const InputField: FC<IInputFieldProps> = ({ label, hasError, errorMessage, ...rest }) => {
+	return (
+		<FormField
+			label={label}
+			render={hasError => (
+				<TextInput style={[styles.container, hasError ? styles.errorBorder : undefined]} {...rest} />
+			)}
+			hasError={hasError}
+			errorMessage={errorMessage}
+		/>
+	);
+};
